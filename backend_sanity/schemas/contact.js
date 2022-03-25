5875a930d6eb4479278bd77dba06449ace35d255
unknown <charafeddine99@hotmail.com>
2022-03-25 14:40:49 +0100

    added contact sheet

--- backend_sanity/schemas/contact.js
+++ backend_sanity/schemas/contact.js
@@ -4,6 +4,11 @@ export default {
     type:'document',
     fields:[
         {
+            name:'contact',
+            title:'Contact',
+            type:'string'
+        },
+        {
             name:'telephone',
             title:'Telephone',
             type:'number',
