1f17894ddeec2b992231f1b8a500a56e703ab026
sma3inn <isma.kabbaj@hotmail.com>
2022-06-20 13:43:27 +0200

    bug fixes

--- frontend_react/src/components/Prestations/Prestations.jsx
+++ frontend_react/src/components/Prestations/Prestations.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import './Prestations.scss';
 
 const Prestations = () => {
   return (
-    <div>
-        <h1>Prestations</h1>
-    </div>
+    <section className='Prestations'>
+      <h2><span className='highlight'>Nos Prestations</span></h2>
+    </section>
   )
 }
 
